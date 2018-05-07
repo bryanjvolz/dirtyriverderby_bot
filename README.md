cff07c0d9997835a499605429118be7b9812c808
Bryan Volz <bryan@typocracy.net>
2018-05-07 00:45:38 -0400

    Add readme, update key/secrets

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dirty River Derby Twitter Reply Bot
+
+Ruby based Twitter reply bot for Heroku based on `https://www.johnpe.art/2017/01/16/its-you-twitter-bot`
+
+This bot runs automatically via the Scheduler addon but can be run manually with `heroku run ruby dirtyriverderbybot.rb`
+
+## To Manage Jobs:
+`heroku addons:open scheduler` // `https://scheduler.heroku.com/dashboard`
--- dirtyriverderbybot.rb
+++ dirtyriverderbybot.rb
@@ -6,10 +6,10 @@ require 'bundler/setup'
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-	config.consumer_key			= ' tVhCE4dDregfjBZO53qzZYNKs'
+	config.consumer_key			= 'tVhCE4dDregfjBZO53qzZYNKs'
 	config.consumer_secret		= 'caPS1gbLN9ZRPPmsap1vedJi6nLuoiOjkXYjjjnxnaqrKxujYJ'
 	config.access_token			= '983719735442509824-kmskSi9RK8RP7feMz4p56oHYBDt0Hf4'
-	config.access_token_secret	= ' XXoIO0mbtZh7hQvvXpb4hV8a4pabRzqLKndGc4j41858R'
+	config.access_token_secret	= 'XXoIO0mbtZh7hQvvXpb4hV8a4pabRzqLKndGc4j41858R'
 end
 
 drd_replies = [
